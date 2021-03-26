730e3c78bb3fae23320a5023bd926a753fe58db0
geomin76 <geom@vt.edu>
2021-03-26 19:41:29 -0400

    Adding changes for photo

--- src/pages/photography.tsx
+++ src/pages/photography.tsx
@@ -3,7 +3,23 @@ import { Header, Container, Segment, Icon } from "semantic-ui-react";
 import {withLayout} from "../components/Layout";
 import Gallery from "react-photo-gallery";
 
-import { photos } from "./photo-data"
+const photos = [
+  {
+      src: "https://lh3.googleusercontent.com/NEHk2UOGnFARpUh2GWs8h4oL9UDquU7vyRc7zJKpUZ93Rz-VwKsFMPv0G0Z1iHOE4tdMqABA-rvdqzL23Xo9yHX8L9mi1Ii4YGaB6eqvzKPGFWZ4QxDTdnitqREd9FXKAumRJdwTAv8=w2400",
+      width: 3,
+      height: 4
+  },
+  {
+      src: "https://lh3.googleusercontent.com/x4VVGYDOWIW0sbkAwWKTbDw5eL-XmUqY3jKgC0N3OoHmh1Ry4MEeNmZXNU8EQ30wluvL_E5osKFgjtYeNSsBlzTVj-a3W-rM36r2aiB80q4TE6RN6cGGU06jtrmhCiuOykg1DNPwB_Y=w2400",
+      width: 4,
+      height: 3
+  },
+  {
+      src: "https://lh3.googleusercontent.com/sIAehltwjACuUeFsCzyHuHZciEPsRmfTgaG2isg-VKUBSUD1hDBtcOAGSzGGKrefQVZoZJTKceMxeEBjADqduxy1ge4YXvQSsu5eBuNsxex4S1bNB6disdXg3MjMApJQ9HGjakJnTkM=w2400",
+      width: 4,
+      height: 3
+  }
+]
 
 const PhotographyPage = () => {
   return (
