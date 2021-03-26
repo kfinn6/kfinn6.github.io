451d28c3ab61740f191ca7470f0fad7670488b91
geomin76 <geom@vt.edu>
2021-03-25 20:19:24 -0400

    Updates

---
+++ component---src-pages-travel-tsx-9efdc750518819b56b80.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"21BM":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),c=n("yE/o"),l=n("3mGJ"),o=n("aQu0"),r=n("soUV");t.default=Object(r.b)((function(){return a.createElement(c.a,null,a.createElement(l.a,{vertical:!0},a.createElement(o.a,{as:"h2"},a.createElement(o.a.Content,null,"Coming soon!"))),a.createElement(l.a,{vertical:!0}))}))}}]);
+//# sourceMappingURL=component---src-pages-travel-tsx-9efdc750518819b56b80.js.map
